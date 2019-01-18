efbdbf5b203e9bb4c5947c90e1fe20fb01fa773c
piratevax <leborgne.pirate@yahoo.fr>
2019-01-18 15:32:48 +0100

    Ajout recherche KEGG

--- src/tuto_ClusterProfiler.R
+++ src/tuto_ClusterProfiler.R
@@ -65,7 +65,7 @@ ego2 <- enrichGO(gene         = gene.df$ENSEMBL,
                  qvalueCutoff  = 0.05)
 ego2 <- setReadable(ego2, OrgDb = org.Hs.eg.db) # map gene ID to gene symbol
 
-# GO Gene Set Enrichment Analysis
+## GO Gene Set Enrichment Analysis
 ego3 <- gseGO(geneList     = geneList,
               OrgDb        = org.Hs.eg.db,
               ont          = "CC",
@@ -74,5 +74,38 @@ ego3 <- gseGO(geneList     = geneList,
               maxGSSize    = 500,
               pvalueCutoff = 0.05,
               verbose      = FALSE)
+
+
+# Kegg Analysis
+
+search_kegg_organism('ece', by='kegg_code')
+ecoli <- search_kegg_organism('Escherichia coli', by='scientific_name')
+dim(ecoli)
+head(ecoli)
+
+## KEGG over-representation test
+kk <- enrichKEGG(gene         = gene,
+                 organism     = 'hsa',
+                 pvalueCutoff = 0.05)
+head(kk)
+
+## KEGG Gene Set Enrichment Analysis
+kk2 <- gseKEGG(geneList     = geneList,
+               organism     = 'hsa',
+               nPerm        = 1000,
+               minGSSize    = 120,
+               pvalueCutoff = 0.05,
+               verbose      = FALSE)
+head(kk2)
+
+## KEGG Module over-representation test
+### KEGG module : collection of manually defined function units
+mkk <- enrichMKEGG(gene = gene,
+                   organism = 'hsa')
+
+## KEGG Module Gene Set Enrichment Analysis
+
+mkk2 <- gseMKEGG(geneList = geneList,
+                 species = 'hsa')
 
 
