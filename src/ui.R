962ae1c31bc4dc9146c998fff0bdb325abc4bdfd
sophiacb <37294517+sophiacb@users.noreply.github.com>
2019-01-18 15:13:10 +0100

    Add files via upload
    ui.v3

--- src/ui.R
+++ src/ui.R
@@ -5,19 +5,20 @@
 # Author :
 #   Sophia ACHAIBOU
 #   Joris ARGENTIN
-#   Xavier BUSSELL
+#   Xavier BUSSEL
 #   Hatim EL JAZOULI
 
 
 
 library(shiny)
 library(shinythemes)
+library(DT)
 
 # Define UI for data upload app ----
 ui <- fluidPage( 
 
   # App title ----
-  titlePanel("Annotation Enrichment Analysis"),
+  titlePanel("Bioinformatics Interactive Toolbox for Enrichment analysis"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -31,20 +32,10 @@ ui <- fluidPage(
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")),
-      
-      # Input: Checkbox if file has header ----
-      checkboxInput("header", "Header", TRUE),
+    
       
       tags$hr(),
       
-      # Input: Select organism ----
-      radioButtons("org", "Source :",
-                   choices = list("BiomaRt" = 1,
-                               "ClusterProfiler" = 2,
-                               "Pathview" = 3 ), 
-                   selected = 1),
-                
-      tags$hr(), 
       
       # Input: Select ID gene source ----
       radioButtons("IDsource", "ID gene source",
@@ -54,8 +45,21 @@ ui <- fluidPage(
       
       # Horizontal line ----
       tags$hr(),
+      selectInput("db", "Species :", 
+                choices=list("Human"="Human",
+                             "Mouse"="Mouse", 
+                             "Other"="Other extracted from BioMart"
+                  )),
+      tags$hr(),
+
+      tabsetPanel(
+        id = 'queue',
+        tabPanel("Submitted jobs", DT::dataTableOutput("queue"))
+      ),
+
+     
+        actionButton("goButton", "GO !")
       
-      actionButton("goButton", "Go!")
     ),
    
       
@@ -63,104 +67,116 @@ ui <- fluidPage(
       # Main panel for displaying outputs ----
       mainPanel(
         tabsetPanel(
-          tabPanel("WDI",
+          tabPanel("Whole Data Inspection",
                   
-                   verbatimTextOutput("orgName"), #a retirer
-                   radioButtons("EA", "Enrichment algorithm :",
-                       choices = list("SEA" = 1,
-                                      "GSEA" = 2,
-                                      "MEA" = 3 ), 
-                       selected = 1),
-                  tags$hr(),
+                   tags$hr(),
+              
                   checkboxGroupInput("checkbox", "Output :",
-                                     c("Vulcano plot"="vulcano",
+                                     c("Volcano plot"="volcano",
                                        "MA plot"="MAplot")),
                   tags$hr(),
-                  actionButton("submitWDI", "SUBMIT")
+                  
+                  div(style="display:inline-block",
+                      sliderInput("FC", "log Fold-change:",
+                                  min = 0, max = 10,
+                                 value = 2, step = 1)
                   ),
-          tabPanel("GOE",
+                  
+                  div(style="display:inline-block",
+                     checkboxGroupInput("cbAdjust", "",
+                                     c("Absolute value"="Absolute value"))
+                  ),
+          
+                  tags$hr(),
+                  sliderInput("p-value", "p-value:",
+                              min = 0, max = 0.1,
+                              value = 0.05, step = 0.01),
+                  tags$hr(),
+                  radioButtons("Am", "Adjustment method:",
+                               choices = list("Bonferroni" = 1,
+                                              "Benjamini-Hochberg" = 2
+                               ), 
+                               selected = 1),
+                  actionButton("submitWDI", "Submit")
+                  ),
+          
+          tabPanel("GO-Enrichment",
                    #textInput("database", label = h3("Database"), value = "Enter database..."),
                    #tags$hr(),
                    selectInput("db", "Database :", 
-                               choices=list("DAVID" = "david",
-                                            "Gene Ontology" = "go",
-                                            "KEGG" = "kegg",
-                                            "MSigDB" = "msig",
-                                            "PANTHER" = "pant",
-                                            "Reactome" = "react",
-                                            "RegulonDB" = "regul",
-                                            "STRINGDB" = "string"
+                               choices=list("GO"="GO",
+                                            "autre"="autre banque ?"
                                )),
                    tags$hr(),
-                   checkboxGroupInput("checkbox", "GO term level :",
+                   checkboxGroupInput("checkbox", "GO term ontology :",
                                       c("Molecular function"="F",
                                         "Cellular component"="C",
                                         "Biological process"="P")),
                    tags$hr(),
                    radioButtons("EA", "Enrichment algorithm :",
                                 choices = list("SEA" = 1,
-                                               "GSEA" = 2,
-                                               "MEA" = 3 ), 
+                                               "GSEA" = 2
+                                              ), 
                                 selected = 1),
                    tags$hr(),
                    radioButtons("methode", "Statistical methods :",
                                 choices = list("X²-test" = 1,
                                                "Fisher's exact test" = 2,
-                                               "Binomial probability" = 3,
+                                               "Binomial test" = 3,
                                                "Hypergeometric test" = 4), 
                                 selected = 1),
                    tags$hr(),
-                   actionButton("submitGOE", "SUBMIT")
+                   actionButton("submitGOE", "Submit")
                    
                    
                    ),
           tabPanel("Pathway",
                    selectInput("db", "Database :", 
-                               choices=list("DAVID" = "david",
-                                            "Gene Ontology" = "go",
-                                            "KEGG" = "kegg",
-                                            "MSigDB" = "msig",
-                                            "PANTHER" = "pant",
-                                            "Reactome" = "react",
-                                            "RegulonDB" = "regul",
-                                            "STRINGDB" = "string"
+                               choices=list("GO"="GO",
+                                            "autre"="autre banque ?"
                                )),
                    tags$hr(),
                    radioButtons("EA", "Enrichment algorithm :",
                                 choices = list("SEA" = 1,
-                                               "GSEA" = 2,
-                                               "MEA" = 3 ), 
+                                               "GSEA" = 2
+                                               ), 
                                 selected = 1),
                    tags$hr(),
                    radioButtons("methode", "Statistical methods :",
                                 choices = list("X²-test" = 1,
                                                "Fisher's exact test" = 2,
-                                               "Binomial probability" = 3,
+                                               "Binomial test" = 3,
                                                "Hypergeometric test" = 4), 
                                 selected = 1),
                    tags$hr(),
-                   actionButton("submitPathway", "SUBMIT")
+                   actionButton("submitPathway", "Submit")
                    
                    
                    ),
-          tabPanel("Protein",
-                   selectInput("db", "Database :", 
-                               choices=list("DAVID" = "david",
-                                            "Gene Ontology" = "go",
-                                            "KEGG" = "kegg",
-                                            "MSigDB" = "msig",
-                                            "PANTHER" = "pant",
-                                            "Reactome" = "react",
-                                            "RegulonDB" = "regul",
-                                            "STRINGDB" = "string"
-                               )),
+          tabPanel("Protein domains",
+                   #selectInput("db", "Database :", 
+                   #           choices=list("GO"="GO",
+                   #                          "autre"="autre banque ?"
+                   #            )),
                    tags$hr(),
+                   radioButtons("EA", "Enrichment algorithm :",
+                                choices = list("SEA"=1,
+                                "GSEA" = 2),
+                                selected = 1),
+                   tags$hr(),
+                 
                    radioButtons("methode", "Statistical methods :",
-                                choices = "SEA"),
-                   tags$hr(),
-                   actionButton("submitProtein", "SUBMIT")
+                                choices = list("X²-test" = 1,
+                                               "Fisher's exact test" = 2,
+                                               "Binomial test" = 3,
+                                               "Hypergeometric test" = 4), 
+                                selected = 1),
+                   actionButton("submitProtein", "Submit"),
+                   tags$hr()
                    
                    )
+
+          
         )
       )
     )
