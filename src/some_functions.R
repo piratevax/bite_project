d5cc2e3494e6c41fea5f5f5822662379160e5d09
piratevax <leborgne.pirate@yahoo.fr>
2019-02-25 11:08:28 +0100

    AjoutFonctionFiltrageSurPvalEtL2FC.AjoutFonctionDEBUG

--- src/some_functions.R
+++ src/some_functions.R
@@ -11,3 +11,18 @@ readFile <- function(path) {
   }
 }
 
+getFilteredSet <- function(tab, pval, l2FC) {
+  read <- read[c(which(read[,3] > l2FC),which(read[,3] < -l2FC)),]
+  read <- read[which(read[,7] < pvalFilter),]
+  return(read)   
+}
+
+DEBUG <- function() {
+  path <- "DE2.TH_ccRCC_checkpoints_from_all.csv"#kidney_tumour_ppl_stage.csv"
+  read <- readFile(path)
+  pvalFilter <- 0.05
+  l2FC <- 1
+  getFilteredSet(read, pvalFilter, l2FC)
+}
+
+DEBUG()
