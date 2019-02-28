ce088eb6bb108c1301107a1da73afae799c22769
piratevax <leborgne.pirate@yahoo.fr>
2019-02-28 15:47:00 +0100

    Préparation pour la lecture de fichier et écout du bouton 'Go'

--- src/server.R
+++ src/server.R
@@ -1,7 +1,7 @@
 #
 # This is the server logic of a Shiny web application. You can run the 
 # application by clicking 'Run App' above.
-#
+#d  
 # Find out more about building applications with Shiny here:
 # 
 #    http://shiny.rstudio.com/
@@ -9,14 +9,38 @@
 
 library(shiny)
 library(shinythemes)
-source("src/some_functions.R")
+source("some_functions.R")
 
-function(input, output) {
+shinyServer(function(session, input, output) {
+  DEBUG.var = TRUE
   
-  output$orgName <- renderPrint({
-    ncount <- input$org
-    paste(ncount)
-  })
+  # output$orgName <- renderPrint({
+  #   ncount <- input$org
+  #   paste(ncount)
+  # })
   #input$goButton
-
-}
+  rv <- reactiveValues()
+  rv$null <- "Files must be loaded"
+  
+  file1 <- reactive({
+    if (!is.null(input$file1)) {
+      readFile(input$file1$datapath, header = input$header)
+    } else {
+      NULL
+    }
+  })
+  organismSelected <- reactive({
+    input$db
+  })
+  observeEvent(
+    input$goButton, {
+      rv$read <- file1()
+      rv$organismSelected <- organismSelected()
+      if (is.null(rv$read)) {
+        ### TODO : update n'apparait pas
+        updateSelectInput(session, "file1", choices="Files must be loaded")
+        if (DEBUG.var)
+          cat("Files must be loaded\n")
+      }
+    })
+})
