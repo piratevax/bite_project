84eaff686a5dccd15e155332936810806348109b
piratevax <leborgne.pirate@yahoo.fr>
2019-02-28 15:48:51 +0100

    Mise en commentaire du code inutile

--- src/some_functions.R
+++ src/some_functions.R
@@ -1,13 +1,13 @@
 library(stringr)
 
-readFile <- function(path) {
-  if (! (str_detect(path, ".csv$") || str_detect(path, ".tsv$"))) {
-    stop("File not csv or tsv!")
-  }
+readFile <- function(path, header) {
+  # if (! (str_detect(path, ".csv$") || str_detect(path, ".tsv$"))) {
+  #   stop("File not csv or tsv!")
+  # }
   if (str_detect(path, ".csv$")) {
-    return(read.csv(path, header = TRUE))
+    return(read.csv(path, header = header))
   } else {
-    return(read.table(path, header = TRUE, sep = "\t"))
+    return(read.table(path, header = header, sep = "\t"))
   }
 }
 
@@ -19,7 +19,7 @@ getFilteredSet <- function(tab, pval, l2FC) {
 
 DEBUG <- function() {
   path <- "DE2.TH_ccRCC_checkpoints_from_all.csv"#kidney_tumour_ppl_stage.csv"
-  read <- readFile(path)
+  read <- readFile(path, TRUE)
   pvalFilter <- 0.05
   l2FC <- 1
   getFilteredSet(read, pvalFilter, l2FC)
